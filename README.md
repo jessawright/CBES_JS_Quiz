a4426a3c929b9b424f131b4281d8c9da46e44d38
ymostafa <ymostafa@LATR-YMOSTAFA1>
2018-01-23 14:59:43 -0600

    Initial Commit

--- README.md
+++ README.md
@@ -1,1 +1,40 @@
 # CBES_JS_Quiz
+
+Please Fork this quiz into your own repository, upload your answers into your repo, and send us the link!
+
+# Installation
+-------------------------
+1 - Download & Install Node (Only using this for npm and running the program) https://nodejs.org/en/download/
+2 - Run "npm install" in the directory where you placed the code
+
+# Run
+-------------------------
+1 - Run "node app.js" in the directory where you placed the code
+2 - Navigate to http://localhost:3007/ in the browser of your choice
+
+# Expected Behavior
+-------------------------
+This very simple grocery list functions as follows...
+ - Imports a collection of groceries from a .json file
+ - Displays those items to the page
+   - If a item costs nothing it will display the cost as "FREE!"
+   - Costs are displayed with a $
+   - Dates are displayed as MM-DD-YYYY (does not include datepicker)
+ - A date is shown in an input box
+ - Clicking in that input box will open a date picker
+ - That date picker will highlight dates where groceries are to expire in red
+ - Displays the total number of items in the grocery list
+ - Displays the total cost of items in the grocery list
+ - Displays the average cost of items in the grocery list
+ - Exceptions are logged to the node/cmd console
+
+
+# Questions/Tasks
+---------------------------
+1 - Add a new item to the grocery list .json, have it expire on 1/21/2019
+2 - There are a couple of pieces of the expected behavior that are not performing as described above. What are they?
+3 - Fix the issues you found in item 2.
+4 - The variable groceryCount is declared at the last line of app.get, why does it still work?
+5 - Add code that calculates a 9.75% sales tax on all groceries, and an 11% sales tax on alcohol (reflect this on the page somehow).
+
+Feel free to add styles to the CSS to make this look better!
---
+++ app.js
@@ -0,0 +1,111 @@
+/*
+	Simple static grocery list program.
+	Using express - app.get will be the entry point.
+*/
+
+var path = require('path')
+var express = require('express');
+var groceryList = require('./groceries.json')
+var app = express();
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'node_modules')));
+
+function readExpirationDates(groceries) {
+	var eventsArray = [];
+  	var loopCounter;
+  	for (loopCounter = 0; loopCounter < groceries.length; loopCounter++) {
+  		eventsArray.push('\'' + groceries[loopCounter].expiration + '\'');
+  	}
+  	return eventsArray;
+}
+
+function formatGroceryText(name, price, expiration) {
+	var formattedName = name;
+	var formattedPrice = '';
+	if (price === 0) {
+		formattedPrice = 'FREE!';
+	} else {
+		formattedPrice = '$' + price;
+	}
+
+	var msec = Date.parse(expiration);
+	var newDate =  new Date(msec);
+	var formattedExpiration = `${newDate.getMonth() + 1}-${newDate.getDate()}-${newDate.getFullYear()}`
+	var formattedResult = formattedName + ': ' + formattedPrice + ' - Expires On: ' + formattedExpiration;
+	return formattedResult;
+}
+
+function createGroceryHTML(groceries) {
+	var groceryHTML = '';
+	var loopCounter; 
+	for(loopCounter = 0; loopCounter < groceries.length; loopCounter++) {
+		var currentGrocery = groceries[loopCounter];
+		var groceryText = formatGroceryText(currentGrocery.name, currentGrocery.price, currentGrocery.expiration);
+		groceryHTML += (`<div id="grocery-list">${groceryText}</div>`);
+	}
+
+	return groceryHTML;
+}
+
+function calculateCosts(groceries) {
+	var costs = {
+		"total": 0,
+		"average": 0
+	}
+	var loopCounter; 
+	for(loopCounter = 0; loopCounter < groceries.length; loopCounter++) {
+		var currentGrocery = groceries[loopCounter];
+		costs.total = costs.total + parseFloat(currentGrocery.price, 10);
+	}
+	costs.average = costs.total/loopCounter;
+	return costs;
+}
+
+//Entry point in the code
+app.get('/', function (req, res) {
+	try {
+			var groceries = groceryList.groceries;
+			var expirationEvents = readExpirationDates(groceries);
+			var listOfGroceries = createGroceryHTML(groceries);
+			var groceryCosts = calculateCosts(groceries);
+			groceryCount = groceries.length;
+			var html = ('<link rel="stylesheet" type="text/css" href="css/site.css">');
+			html += ('<link rel="stylesheet" type="text/css" href="css/pikaday.css">');
+			html += ('<div id="welcome">Welcome</div>');
+			html += ('<div id="grocery-list">Grocery List</div><br>');
+			html += listOfGroceries;
+			html += (`<br>
+				  <input type="text" id="datepicker">`);
+
+			html += (`<script src="pikaday/pikaday.js"></script>
+						    <script>
+
+						    var picker = new Pikaday(
+						    {
+						    	defaultDate: new Date(2019, 00, 13),
+						    	setDefaultDate: true,
+						        field: document.getElementById('datepicker'),
+						        firstDay: 1,
+						        ${expirationEvents.length ? 'events:[' + expirationEvents + '],' : ''}
+						        minDate: new Date(2019, 00, 01),
+						        maxDate: new Date(2020, 11, 31),
+						        yearRange: [2000,2020]
+						    });
+
+						    </script>`);
+
+			html += (`<div>Total Number of Groceries on List is ${groceryCount}</div>`);
+			html += (`<div>Total Cost of Groceries on List is $${groceryCosts.total.toFixed(2)}</div>`);
+			html += (`<div>Average Cost of Groceries on List is $${groceryCosts.average.value.toString().toFixed(2)}</div>`);
+	}
+	catch(e) {
+		console.log(e);
+	}
+  	res.send(html);
+  	var groceryCount;
+});
+
+app.listen(3007, function () {
+  	console.log('Running on port 3007');
+});
